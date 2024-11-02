7cf28c546d369b82ddb0894c7f3f41c5e10f9f46
nickspoto <nickspoto1221@gmail.com>
2024-11-02 17:40:50 -0400

    Minor padding issue

--- frontend/src/pages/Weather.tsx
+++ frontend/src/pages/Weather.tsx
@@ -4,24 +4,24 @@ import { coinflip } from "@full-stack/common";
 import { BACKEND_BASE_PATH } from "../constants/Navigation";
 
 const getWeather = (): Promise<WeatherResponse> =>
-    fetch(`${BACKEND_BASE_PATH}/weather`).then((res) => res.json());
+  fetch(`${BACKEND_BASE_PATH}/weather`).then((res) => res.json());
 
 const Weather = () => {
-    const [{ raining }, setRaining] = useState<WeatherResponse>({
-        raining: coinflip(),
-    });
+  const [{ raining }, setRaining] = useState<WeatherResponse>({
+    raining: coinflip(),
+  });
 
-    useEffect(() => {
-        console.log("Loading weather...");
-        getWeather().then((data) => setRaining(data));
-    }, []);
+  useEffect(() => {
+    console.log("Loading weather...");
+    getWeather().then((data) => setRaining(data));
+  }, []);
 
-    return (
-        <div>
-            <h1>Is it raining in New York?</h1>
-            <p>{raining ? "Yes" : "No"}</p>
-        </div>
-    );
+  return (
+    <div style={{ paddingLeft: "15px" }}>
+      <h1>Is it raining in New York?</h1>
+      <p>{raining ? "Yes" : "No"}</p>
+    </div>
+  );
 };
 
 export default Weather;
